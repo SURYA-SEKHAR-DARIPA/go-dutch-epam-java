3076026568a54fd35e0059d23d293a3f08280026
Surya <suryasekhardaripa60461@gmail.com>
2023-01-23 23:31:16 +0530

    GoDutch solved

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -1,8 +1,22 @@
 package com.epam.rd.autotasks.godutch;
+import java.util.*;
 
 public class GoDutch {
 
     public static void main(String[] args) {
         //Write code here
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a<0){
+            System.out.println("Bill total amount cannot be negative");
+        }
+        else if(b<=0){
+            System.out.println("Number of friends cannot be negative or zero");
+        }
+        else{
+            int total=a+((a*10)/100);
+            System.out.println(total/b);
+        }
     }
 }
